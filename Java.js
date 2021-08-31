b83b05968ffeb0d88a97a2a38595753a71eff53e
MD. RAKIBUL HASAN <79636944+rakibulhasan15@users.noreply.github.com>
2021-08-31 20:07:21 +0600

    html practice

---
+++ Java.js
@@ -0,0 +1,4 @@
+document.getElementById("test").innerHTML ="Hello World";
+document.getElementById("test").style.color = "white";
+document.getElementById("test").style.fontSize ="50px";
+document.getElementById("test").style.backgroundColor ="red"
